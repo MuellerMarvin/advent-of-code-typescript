485e5bafa1ddba0e5a6c157b2cbccc0662df322d
Marvin Müller <marvin.mueller@code.berlin>
2023-12-04 18:23:06 +0100

    Set up day 2

---
+++ 2023/src/day02/index.ts
@@ -0,0 +1,47 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => {
+  return rawInput.split(": ")[1];
+};
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const getGames = (input: string) => {
+  const lines = input.split("\n");
+
+  // TODO
+
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
