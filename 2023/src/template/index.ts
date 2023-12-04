a07781369cc35336e303b59dda327c8809abfe83
Marvin Müller <marvin.mueller@code.berlin>
2023-12-05 00:53:50 +0100

    Explicitly return any from puzzle template

--- 2023/src/template/index.ts
+++ 2023/src/template/index.ts
@@ -2,13 +2,13 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
-const part1 = (rawInput: string) => {
+const part1 = (rawInput: string): any => {
   const input = parseInput(rawInput);
 
   return;
 };
 
-const part2 = (rawInput: string) => {
+const part2 = (rawInput: string): any => {
   const input = parseInput(rawInput);
 
   return;
