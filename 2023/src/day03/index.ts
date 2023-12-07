9642949660a8245c1ad055f4c25208dbca629fec
Marvin Müller <marvin.mueller@code.berlin>
2023-12-07 02:52:40 +0100

    Setup day 3

--- 2023/src/day03/index.ts
+++ 2023/src/day03/index.ts
@@ -1,16 +1,14 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => {
+  let lines: any = rawInput.split('\n');
+  lines = lines.map((line) => Array.from(line));
+  return lines;
+};
 
 const part1 = (rawInput: string): any => {
   const input = parseInput(rawInput);
-  const lines = input.split("\n");
-  const lineNumbers: NumberPos[][] = lines.map((line) =>
-    findNumbersInLine(line),
-  );
-  const partNumbers: number[] = 
-
-  return partNumbers.reduce((acc, value) => acc + value, 0);
+  return;
 };
 
 const part2 = (rawInput: string): any => {
@@ -19,58 +17,8 @@ const part2 = (rawInput: string): any => {
   return;
 };
 
-type NumberPos = {
-  value: number;
-  index: number;
-  endIndex: number;
-};
-
-const findNumbersInLine = (line: string): NumberPos[] => {
-  let i = 0;
-  let numbers: NumberPos[] = [];
-  while (i < line.length) {
-    let nextNum: NumberPos = findNextNumber(line, i);
-    if (nextNum.index !== null) {
-      numbers.push(nextNum);
-    } else {
-      break;
-    }
-    i = nextNum.endIndex;
-  }
-  return numbers;
-};
-
-const findNextNumber = (line: string, startIndex: number): NumberPos => {
-  let match = "";
-  let index = startIndex;
-  let collecting = false;
-  for (let i = startIndex; i < line.length; i++) {
-    if (line[i].match(/\d/)) {
-      match += line[i];
-      if (!collecting) index = i;
-      collecting = true;
-      continue;
-    }
-    if (collecting) break; // If it is collecting but didn't find a digit anmore
-  }
-  if (match === "") return { value: null, index: null, endIndex: null };
-  return {
-    value: parseInt(match),
-    index: index,
-    endIndex: index + match.length,
-  };
-};
-
-const containsSymbol = (text: string): boolean => {
-  return !Array.from(text).every((value) => {
-    return !isSymbol(value);
-  });
-}
-
-const isSymbol = (value: string): boolean => {
-  if(value.match(/\d/) !== null || value == '.') return true;
-  return false;
-}
+// getIfNumberHasSymbolsAroundit()
+// getNumberFromFirstDigit()
 
 run({
   part1: {
