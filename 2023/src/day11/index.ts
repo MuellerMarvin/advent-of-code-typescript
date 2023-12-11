bd947a1844de7795394443afc8cbd24bc5a312bb
Marvin Müller <marvin.mueller@code.berlin>
2023-12-11 18:23:00 +0100

    Add space expansion, day 11

--- 2023/src/day11/index.ts
+++ 2023/src/day11/index.ts
@@ -1,13 +1,14 @@
 import run from "aocrunner";
 
 const parseInput = (rawInput: string) => {
-  const lines: string[] = rawInput.split('\n')
-  return lines;
+  const lines: string[] = rawInput.split("\n");
+  return { grid: lines.map((line) => Array.from(line)) };
 };
 
 const part1 = (rawInput: string): any => {
-  const input = parseInput(rawInput);
-
+  const grid = parseInput(rawInput).grid;
+  const galaxies = getGalaxies(grid);
+  const space = expandSpace(grid);
   return;
 };
 
@@ -15,6 +16,44 @@ const part2 = (rawInput: string): any => {
   const input = parseInput(rawInput);
 
   return;
+};
+
+const getGalaxies = (grid: string[][]) => {
+  let galaxies = [];
+  grid.forEach((line, lineIndex) => {
+    line.forEach((char, charIndex) => {
+      if (char === "#") {
+        galaxies.push([lineIndex, charIndex]);
+      }
+    });
+  });
+
+  return galaxies;
+};
+
+const expandSpace = (inputGrid: string[][]) => {
+  // Deep copy to preverse input-grid
+  let grid = inputGrid.map((line) => line.map((char) => char));
+
+  //#region Expand Lines
+  let emptyLines: number[] = [];
+  grid.forEach((line, lineIndex) => {
+    if (line.every((value) => value === ".")) {
+      emptyLines.push(lineIndex);
+    }
+  });
+
+  for (let i = 0; i < emptyLines.length; i++) {
+    grid.splice(emptyLines[i], 0, Array(grid[i].length).fill("."));
+    emptyLines = emptyLines.map((value) => value + 1);
+  }
+  //#endregion Expand Lines
+
+  //#region Expand Columns
+
+  //#endregion Expand Columns
+
+  return grid;
 };
 
 run({
