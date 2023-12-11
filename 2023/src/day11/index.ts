7c85116ad8a85c9806aae7dd4f310ca5bb9908b8
Marvin Müller <marvin.mueller@code.berlin>
2023-12-11 20:12:23 +0100

    Solve Day 11 Part 1

--- 2023/src/day11/index.ts
+++ 2023/src/day11/index.ts
@@ -3,14 +3,22 @@ import { getColumn } from "../utils/index.js";
 
 const parseInput = (rawInput: string) => {
   const lines: string[] = rawInput.split("\n");
-  return { grid: lines.map((line) => Array.from(line)) };
+  const grid: string[][] = lines.map((line) => Array.from(line))
+  return { grid };
 };
 
 const part1 = (rawInput: string): any => {
   const grid = parseInput(rawInput).grid;
-  const galaxies = getGalaxies(grid);
   const space = expandSpace(grid);
-  return;
+  let galaxies = getGalaxies(space);
+
+  let distSum = 0;
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      distSum += getDistance(galaxies[i], galaxies[j]);
+    }
+  }
+  return distSum;
 };
 
 const part2 = (rawInput: string): any => {
@@ -19,7 +27,7 @@ const part2 = (rawInput: string): any => {
   return;
 };
 
-const getGalaxies = (grid: string[][]) => {
+const getGalaxies = (grid: string[][]): number[][] => {
   let galaxies = [];
   grid.forEach((line, lineIndex) => {
     line.forEach((char, charIndex) => {
@@ -34,7 +42,7 @@ const getGalaxies = (grid: string[][]) => {
 
 const expandSpace = (inputGrid: string[][]) => {
   // Deep copy to preverse input-grid
-  let grid = inputGrid.map((line) => line.map((char) => char));
+  let grid: string[][] = inputGrid.map((line) => line.map((char) => char));
 
   //#region Expand Lines
   let emptyLines: number[] = [];
@@ -53,7 +61,8 @@ const expandSpace = (inputGrid: string[][]) => {
   //#region Expand Columns
   let emptyColumns: number[] = [];
   for (let i = 0; i < grid[0].length; i++) {
-    if(getColumn(grid, i).every((value) => value === '.')) {
+    const column = getColumn(grid, i)
+    if (column.every((value) => value === ".")) {
       emptyColumns.push(i);
     }
   }
@@ -61,22 +70,28 @@ const expandSpace = (inputGrid: string[][]) => {
   for (let index = 0; index < emptyColumns.length; index++) {
     // Fill one element into each line at the index of the empty colunmn
     for (let line = 0; line < grid.length; line++) {
-      grid[line].splice(emptyColumns[index], 0, '.');
-      
+      grid[line].splice(emptyColumns[index], 0, ".");
     }
+    emptyColumns = emptyColumns.map((value) => value + 1);
   }
   //#endregion Expand Columns
 
   return grid;
 };
 
+const getDistance = (a: number[], b: number[]) => {
+  let lineDistance = Math.max(a[0], b[0]) - Math.min(a[0], b[0]);
+  let columnDistance = Math.max(a[1], b[1]) - Math.min(a[1], b[1]);
+  return lineDistance + columnDistance;
+};
+
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `...#......\n.......#..\n#.........\n..........\n......#...\n.#........\n.........#\n..........\n.......#..\n#...#.....`,
+        expected: 374,
+      },
     ],
     solution: part1,
   },
